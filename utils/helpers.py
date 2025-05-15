37bfe39338b03e1e7a48f07b0ee5a1a66e0901aa
Vaishnawe Kandikonda <vaishnavi.kandikonda@ucdconnect.ie>
2025-05-15 19:22:39 +0100

    cmmit

--- utils/helpers.py
+++ utils/helpers.py
@@ -152,12 +152,11 @@ def reset_progress(sections):
             del st.session_state[checkbox_key]  # Remove the checkbox state
 
     # Reset the read sections set for the current page
-    page_key = f"{sections}_read_sections"
-    st.session_state[page_key] = set()
+    st.session_state["read_sections"] = set()
 
     # Save progress to file
     save_progress()
-
     # Notify the user and rerun the app
     st.success("Progress reset! All checkboxes have been cleared.")
+    
     st.rerun()
