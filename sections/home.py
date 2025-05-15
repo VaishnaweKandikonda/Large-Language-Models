959ce763b1a954b889330040f193993218c147b0
Vaishnawe Kandikonda <vaishnavi.kandikonda@ucdconnect.ie>
2025-05-15 20:49:51 +0100

    h

--- sections/home.py
+++ sections/home.py
@@ -91,8 +91,6 @@ def render():
 
     # --- Sub-topic selector ---
     col_left, col_right = st.columns([3, 1])
-    with col_left:
-        st.markdown("### Explore Key Sections")
     with col_right:
         default_index = 0  # "All" is the first item
         subtopic = st.selectbox("Sub-topic", ["All"] + list(home_sections.keys()), key="Sub-topic", index=default_index)
